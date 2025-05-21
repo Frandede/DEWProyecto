0d8d10257c08ef9fbe627927e73d19b221fa18a0
Aaron <dew@dew-amongom1-2425>
2025-05-21 13:39:57 +0200

    Prueba

--- src/main/java/filtros/Logs.java
+++ src/main/java/filtros/Logs.java
@@ -18,6 +18,7 @@ public class Logs implements Filter {
         }
     }
 
+    
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
             throws IOException, ServletException {
